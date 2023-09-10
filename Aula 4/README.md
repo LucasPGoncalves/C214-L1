b3f9bb717db5886545d09644e39854a2d1d8c8d1
Lucas Goncalves <l.pereira@ges.inatel.br>
2023-09-10 16:22:39 -0300

    Aula 4

---
+++ Aula 4/README.md
@@ -0,0 +1,34 @@
+# Calculadora de IMC
+
+Este é um projeto de uma aplicação simples para calcular e monitorar o Índice de Massa Corporal (IMC) de um usuário. O IMC é uma medida que relaciona o peso e a altura de uma pessoa, fornecendo uma indicação geral sobre a adequação do peso.
+
+## Como Funciona
+
+1. Insira seu peso em quilogramas (kg) no campo designado.
+2. Insira sua altura em metros (m) no campo designado.
+3. Clique no botão "Calcular IMC".
+4. O aplicativo calculará automaticamente o seu IMC com base nos valores inseridos.
+5. O IMC calculado será exibido junto com uma descrição do seu status de peso.
+
+## Resultados do IMC
+
+O aplicativo fornece uma avaliação do seu IMC com base nos seguintes intervalos:
+
+- **Abaixo de 18.5:** Abaixo do peso.
+- **18.5 a 24.9:** Peso ideal.
+- **25.0 a 29.9:** Levemente acima do peso.
+- **30.0 a 34.9:** Obesidade grau I.
+- **35.0 a 39.9:** Obesidade grau II.
+- **40.0 ou mais:** Obesidade grau III.
+
+## Histórico de IMC
+
+O aplicativo também mantém um histórico de IMC calculados, mostrando os valores anteriores e a data em que foram calculados e mostra em grafico.
+
+## Como Executar o Projeto
+
+1. Certifique-se de ter o Node.js instalado em seu sistema.
+2. Faça o download ou clone este repositório.
+3. Abra o terminal e navegue até o diretório do projeto.
+4. Execute o seguinte comando no terminal para baixar as dependencias: npm i
+5. Execute o programa pelo terminal digitando: npm run dev
---
+++ Aula 4/app_imc/src/App.svelte
@@ -0,0 +1,155 @@
+<script>
+  import Chart from "chart.js/auto";
+  import { onMount } from "svelte";
+
+  // Variáveis de estado
+  let peso = 0; // Peso do usuário
+  let altura = 0; // Altura do usuário
+  let imc = 0; // Índice de Massa Corporal
+  let historicoIMC = []; // Histórico de Índice de Massa Corporal
+  let historicoIMCAtualizado = [];
+  let historicoDataAtualizado = []
+  let ctx;
+  let myChart;
+
+  function atualizarGrafico() {
+    historicoIMCAtualizado = historicoIMC.map((item) => item.valor);
+	historicoDataAtualizado = historicoIMC.map((item) => item.data);
+	chart();
+  }
+
+  function chart(){
+	if (myChart) {
+      myChart.destroy(); // Destruir o gráfico existente para evitar duplicações
+    }
+    
+    myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: historicoDataAtualizado, // Usar índices como rótulos
+        datasets: [
+          {
+            label: "IMC",
+            data: historicoIMCAtualizado, // Usar a variável atualizada
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  // Função para calcular o IMC
+  function calcularIMC() {
+    if (peso > 0 && altura > 0) {
+      imc = (peso / Math.pow(altura, 2)).toFixed(2);
+      historicoIMC = [...historicoIMC, { valor: imc, data: new Date() }];
+	  atualizarGrafico();
+    } else {
+      imc = 0;
+    }
+  }
+
+  // Função para calcular cor do texto do IMC
+  function calculaCorTexto(imcValor) {
+    const minCor = "#00FF00";
+    const maxCor = "#FF0000";
+    const porcentagem = (imcValor - 18.5) / (45 - 18.5);
+    const r = Math.floor(
+      (1 - porcentagem) * parseInt(minCor.substring(1, 3), 16) +
+        porcentagem * parseInt(maxCor.substring(1, 3), 16)
+    );
+    const g = Math.floor(
+      (1 - porcentagem) * parseInt(minCor.substring(3, 5), 16) +
+        porcentagem * parseInt(maxCor.substring(3, 5), 16)
+    );
+    const b = Math.floor(
+      (1 - porcentagem) * parseInt(minCor.substring(5, 7), 16) +
+        porcentagem * parseInt(maxCor.substring(5, 7), 16)
+    );
+    return `#${r.toString(16).padStart(2, "0")}${g
+      .toString(16)
+      .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
+  }
+
+  onMount(chart);
+</script>
+
+<main>
+  <h1>Calculadora de IMC</h1>
+  <label>
+    Peso: <input type="number" bind:value={peso} /> kg
+  </label>
+  <label>
+    Altura: <input type="number" bind:value={altura} /> m
+  </label>
+  <button on:click={calcularIMC}>Calcular IMC</button>
+  {#if imc > 0}
+    <p>
+      Seu IMC é {@html `<span style="color: ${calculaCorTexto(
+        imc
+      )};">${imc}</span>`}
+    </p>
+    {#if imc < 18.5}
+      <p style="">Abaixo do peso</p>
+    {:else if imc >= 18.5 && imc < 25}
+      <p>Peso ideal (parabéns)</p>
+    {:else if imc >= 25 && imc < 30}
+      <p>Levemente acima do peso</p>
+    {:else if imc >= 30 && imc < 35}
+      <p>Obesidade grau I</p>
+    {:else if imc >= 35 && imc < 40}
+      <p>Obesidade grau II (severa)</p>
+    {:else}
+      <p>Obesidade III (mórbida)</p>
+    {/if}
+  {/if}
+
+  <h2>Histórico de IMC</h2>
+  <ul>
+    {#each historicoIMC as item}
+      <li>
+        Valor do IMC: <span
+          >{@html `<span style="color: ${calculaCorTexto(item.valor)};">${
+            item.valor
+          }</span>`}</span
+        >
+        (Calculado em: {item.data.toLocaleString()})
+      </li>
+    {/each}
+  </ul>
+
+    <canvas id="myChart" width="400" height="100" bind:this={ctx}></canvas>
+</main>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    padding: 2rem;
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  input {
+    padding: 0.25rem;
+  }
+
+  p {
+    text-transform: uppercase;
+  }
+</style>
